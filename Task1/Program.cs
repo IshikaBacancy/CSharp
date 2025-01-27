cdbd62596f798383e09fbf9bc50f7ceda25d5b89
Student ApnaCollege <ishika.rict21@sot.pdpu.ac.in>
2025-01-27 12:55:48 +0530

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+//Console.WriteLine("Hello, World!");
+
+// Method to calculate the factorial of a number
+int Factorial(int n)
+{
+    int res = 1;
+
+    for (int i = 2; i <= n; i++)
+        res *= i;
+    return res;
+}
+
+// Prompt the user to enter a number
+Console.Write("Enter a number to calculate its factorial: ");
+string? input = Console.ReadLine();
+
+// Validate and parse user input
+if (int.TryParse(input, out int num) && num >= 0)
+{
+    Console.WriteLine("Factorial of " + num + " is " + Factorial(num));
+}
+else
+{
+    Console.WriteLine("Invalid input! Please enter a non-negative integer.");
+}
