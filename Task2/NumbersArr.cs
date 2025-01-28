3c3516e494219c2d0aaf0989378599119ff003b4
IshikaBacancy <ishika.raiyani@bacancy.com>
2025-01-28 12:24:46 +0530

    Add files via upload

---
+++ Task2/NumbersArr.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class NumbersArr
+    {
+        public int[] Numbers { get; set; }
+
+        public NumbersArr()
+        {
+            Random random = new Random();
+            Numbers = new int[50];
+
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = random.Next(1, 50);
+
+
+            }
+
+
+        }
+
+        
+        //Calculating the even sum of randomly generated 50 integers 
+        public int CalculateEvenSum()
+        {
+            int sum = 0;
+            foreach (int number in Numbers)
+            {
+                if(number % 2 == 0)
+                {
+                    sum += number;
+                }
+                
+            }
+            return sum;
+
+        }
+         
+        //Calculating the even average of randomly generated 50 integers
+        public double CalculateEvenAverage()
+        {
+            int sum = 0;
+            int count = 0;
+
+            foreach (int number in Numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                    count++;
+                }
+            }
+            return count > 0 ? (double)sum / count : 0;
+        }
+    }
+}
