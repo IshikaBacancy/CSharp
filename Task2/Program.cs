0928cfd12458321c4f615785f2b421f3509cd03b
IshikaBacancy <ishika.raiyani@bacancy.com>
2025-01-30 18:49:00 +0530

    Update Program.cs

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,5 +1,10 @@
+// See https://aka.ms/new-console-template for more information
+
+//Task 2: Create a program that generates 50 random integers, stores them in an array, and calculates and displays both the sum and average of even numbers only
+
 using System;
 using Task2;
+
 
 namespace Array
 {
@@ -7,16 +12,47 @@ namespace Array
     {
         static void Main(String[] args)
         {
-            NumbersArr array = new NumbersArr();
+            try
+            {
+                EvenStatistics array = new EvenStatistics();
+
+                try
+                {
+                    Console.WriteLine("The generated numbers are: " + string.Join(", ", array.Numbers));
+
+                    Console.WriteLine("The even sum of the array: " + array.CalculateEvenSum());
+
+                    Console.WriteLine("The even average of the array: " + array.CalculateEvenAverage());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred while processing the numbers: " + ex.Message);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while initializing the EvenStatistics object: " + ex.Message);
+            }
+                
+
+
+
+
+
 
             
-            
-            Console.WriteLine("The generated numbers are: " + string.Join(", ", array.Numbers));
 
-            Console.WriteLine("The even sum of the array: " + array.CalculateEvenSum());
-
-            Console.WriteLine("The even average of the array: " + array.CalculateEvenAverage());
 
         }
     }
 }
+
+
+
+
+
+
+
+
+
