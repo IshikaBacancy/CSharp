6d74d4a24fded52dce2dbbd254284b3dbda14786
IshikaBacancy <ishika.raiyani@bacancy.com>
2025-01-30 17:19:34 +0530

    Add files via upload

---
+++ Task 4/AsyncTasker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    public class AsyncTasker
+    {
+        public static async Task<int> PerformAsyncTask(int id)
+        {
+            try
+            {
+                Console.WriteLine($"Task {id} started ....");
+                await Task.Delay(new Random().Next(1000, 3000));
+                Console.WriteLine($"Task {id} completed.");
+                return id * 10;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error in Task {id}: {ex.Message}");
+                return -1;
+            }
+
+            
+        }
+    }
+}
