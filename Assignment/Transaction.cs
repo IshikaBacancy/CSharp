f5a02853eabc3cf3e9d62d85779974caa9d02326
IshikaBacancy <ishika.raiyani@bacancy.com>
2025-01-31 10:42:55 +0530

    Add files via upload

---
+++ Assignment/Transaction.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Transaction
+    {
+        //protected string firstName;
+        //protected string lastName;
+        //protected long Accountnumber;
+        //protected int InitialBalance;
+
+        public string FirstName()
+        {
+            string name = "Ishika";
+            //string last_name = "Raiyani";
+
+
+
+
+
+
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+            }
+        
+
+        }
+
+        public long AccountNumber
+        {
+            get { return AccountNumber; }
+            set
+            {
+                AccountNumber = value;
+            }
+
+
+        }
+
+        public int Initialbalance
+        {
+            get { return InitialBalance; }
+            set
+            {
+                InitialBalance = value;
+            }
+
+
+        }
+
+
+    }
+}
