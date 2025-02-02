931c3bbd19caa7ad2e0c1f42927183366c64c5bb
IshikaBacancy <ishika.raiyani@bacancy.com>
2025-02-02 23:31:54 +0530

    Update Program.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,64 +1,133 @@
 
-//Console.WriteLine("Hello, World!");
+
 using System;
-using System.Collections.Generic;
+
 using System.Linq;
 
-
-namespace Assignment
+namespace Practice_1
 {
     class Program
     {
-        
-        
-            try
-            {
+        static void Main(string[] args)
+        {
+
+            bool exit = false;
+            //while(!exit)
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine("Bank Menu:");
+            //    Console.WriteLine("1. Create a new bank account");
+            //    Console.WriteLine("2. View all bank accounts");
+            //    Console.WriteLine("3. Search accounts by name");
+            //    Console.WriteLine("4. Sort accounts by balance (Ascending/Descending)");
+            //    Console.WriteLine("5. Deposit money into an account");
+            //    Console.WriteLine("6. Withdraw money from an account");
+            //    Console.WriteLine("7. View transaction history of an account");
+            //    Console.WriteLine("8. View details of a specific account");
+            //    Console.WriteLine("9. Get total bank balance");
+            //    Console.WriteLine("10. Exit");
+
+            //    Console.Write("Select an option: ");
+            //    string choice = Console.ReadLine();
+
+            //    switch (choice)
+            //    {
+            //        case "1":
+            //           Account.AddAccount();
+            //            break;
+
+            //        case "2":
+            //            Account.ViewAllAccounts();
+            //            break;
+
+            //        case "3":
+            //            Account.SearchAccountByName();
+            //            break;
+
+            //        case "4":
+            //            SortAccountsByBalance();
+            //            break;
+
+            //        case "5":
+            //            Deposit();
+            //            break;
+
+            //        case "6":
+            //            Withdraw();
+            //            break;
+
+            //        case "7":
+            //            ViewTransactionHistory();
+            //            break;
+
+            //        case "8":
+            //            ViewAccountDetails();
+            //            break;
+
+            //        case "9":
+            //            Console.WriteLine($"Total bank balance: {Account.GetTotalBankBalance()}");
+            //            break;
+
+            //        case "10":
+            //            exit = true;
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("Invalid option. Try again.");
+            //            break;
+            //    }
+
+            //}
+            Account account1 = new Account("Ishika Raiyani", 21890, 100000);
+            Account account2 = new Account("Jane Smith", 21980, 500000);
+            Account account3 = new Account("Priya Rajan", 123456, 100000);
+
+            Account.AddAccount(account1);
+            Account.AddAccount(account2);
+            Account.AddAccount(account3);
+
             
+            Console.WriteLine("\nViewing account details for account number 21890:");
+            Account.ViewAccountByNumber(21890);
 
-    
-        
-        
-            static void Main(string[] args)
+            Console.WriteLine($"\nTotal bank balance: {Account.GetTotalBankBalance()}");
+
+            account1.DisplayAccountInfo();
+            account2.DisplayAccountInfo();
+            account3.DisplayAccountInfo();
+
+            account1.Deposit(1000);
+            account2.Deposit(1000);
+
+            account1.Withdraw(2000);
+            account2.Withdraw(45000);
+
+            account1.ViewTransactionHistory();
+            account2.ViewTransactionHistory();
+
+            foreach (var account in Account.bankaccounts)
             {
-                String input;
-               
-                Console.WriteLine("Welcome to Bank Management System");
-                while (true)
-                {
-                    Console.WriteLine("\nWhat you want to do:");
-                    Console.WriteLine("1. Create account");
-                    Console.WriteLine("2. Enter the account number");
-                    
-
-                    object ob1 = Console.ReadLine();
-                    input = Convert.ToString(ob1);
-
-                    if (input == "1")
-                    {
-                        Console.WriteLine("Enter account Type :");
-                        .create_account();
-
-                    }
-                    else if (input == "2")
-                    {
-                        Console.Write("Enter account Number :");
-                        .showInfo();
-                    }
-                    
-                    
-                    Console.ReadKey();
-
-
-                }
-
+                Console.WriteLine($"Account for {account.Name} with Account Number {account.AccountNumber} has an Initial Balance of {account.InitialBalance}");
             }
 
-        
+            Account.ViewAllAccounts();
+
+
+
+            
+            Console.WriteLine("\nSorting accounts by balance in ascending order...");
+            Account.SortAccountsByBalance(true);
+            Account.ViewAllAccounts();
+
+            // Sorting accounts by balance (descending)
+            Console.WriteLine("\nSorting accounts by balance in descending order...");
+            Account.SortAccountsByBalance(false);
+            Account.ViewAllAccounts();
+
+
+        }
+
+
     }
 
 }
-        }
-    
-
-
-
