423a5bcf7c85fe1e4240257cd82b79b71eec90a9
IshikaBacancy <ishika.raiyani@bacancy.com>
2025-02-05 17:05:58 +0530

    Update Program.cs

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -9,22 +9,37 @@ namespace Day_6
         {
             Car car = new Car();
 
-            
+
 
             Car luxuryCar = new Car("Mercedes", "S-Class", 5000.5f, true);
-            
-            Car luxuryCar1 = new Car("BMW", "7 Series", 6000.2f, true);
-            
+            luxuryCar = null;
+            GC.Collect();
+
+            Car luxuryCar1 = new Car("BMW", "7 Series", 6000.2f, false);
+            luxuryCar1 = null;
+            GC.Collect();
+
             Car luxuryCar2 = new Car("Audi", "A8", 5500.8f, false);
+            luxuryCar2 = null;
+            GC.Collect();
 
 
-            
 
             Car economyCar1 = new Car("Toyota", "Corolla", 365.7f, true);
+            economyCar1 = null;
+            GC.Collect();
             Car economyCar2 = new Car("Hyundai", "Elantra", 400.3f, false);
+            economyCar2 = null;
+            GC.Collect();
             Car economyCar3 = new Car("Honda", "Civic", 450.2f, true);
+            economyCar3 = null;
+            GC.Collect();
             Car economyCar4 = new Car("Ford", "Focus", 320.5f, true);
+            economyCar4 = null;
+            GC.Collect();
             Car economyCar5 = new Car("Nissan", "Sentra", 350.8f, false);
+            economyCar5 = null;
+            GC.Collect();
 
             Console.WriteLine("Luxury Cars:");
             luxuryCar.GetDetails();
@@ -39,38 +54,8 @@ namespace Day_6
             economyCar3.GetDetails();
             economyCar4.GetDetails();
             economyCar5.GetDetails();
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
 
     }
 }
-
 
