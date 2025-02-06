81073c4af62fafcc24d556ff5646ffc49d696593
IshikaBacancy <ishika.raiyani@bacancy.com>
2025-02-06 17:11:51 +0530

    Update Program.cs

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -8,64 +8,37 @@ namespace Day_6
         static void Main(string[] args)
         {
             Car car = new Car();
-
-
-
             Car luxuryCar = new Car("Mercedes", "S-Class", 5000.5f, true);
-            luxuryCar.GetRentCar();
-            luxuryCar.SetRentCar("Model A");
-            luxuryCar.GetReturnCar();
-            luxuryCar.SetReturnCar("Model A");
-
-
-
-
-            Car luxuryCar1 = new Car("BMW", "7 Series", 6000.2f, false);
-            luxuryCar1.GetRentCar();
-            luxuryCar.SetRentCar("Model B");
-            luxuryCar.GetReturnCar();
-            luxuryCar.SetReturnCar("Model B");
-
+            luxuryCar.RentCar();
+            luxuryCar.ReturnCar();
+            
+            Car luxuryCar1 = new Car("BMW", "7 Series", 6000.2f, true);
+            luxuryCar1.RentCar();
+            luxuryCar1.ReturnCar();
 
             Car luxuryCar2 = new Car("Audi", "A8", 5500.8f, false);
-            luxuryCar2.GetRentCar();
-            luxuryCar.SetRentCar("Model C");
-            luxuryCar.GetReturnCar();
-            luxuryCar.SetReturnCar("Model C");
-
-
+            luxuryCar2.RentCar();
+            luxuryCar2.ReturnCar();
 
             Car economyCar1 = new Car("Toyota", "Corolla", 365.7f, true);
-            economyCar1.GetRentCar();
-            economyCar1.SetRentCar("Model D");
-            economyCar1.GetReturnCar();
-            economyCar1.SetReturnCar("Model D");
-
+            economyCar1.RentCar();
+            economyCar1.ReturnCar();
 
             Car economyCar2 = new Car("Hyundai", "Elantra", 400.3f, false);
-            economyCar2.GetRentCar();
-            economyCar2.SetRentCar("Model E");
-            economyCar2.GetReturnCar();
-            economyCar2.SetReturnCar("Model E");
+            economyCar2.RentCar();
+            economyCar2.ReturnCar();
 
             Car economyCar3 = new Car("Honda", "Civic", 450.2f, true);
-            economyCar3.GetRentCar();
-            economyCar3.SetRentCar("Model F");
-            economyCar3.GetReturnCar();
-            economyCar3.SetReturnCar("Model F");
+            economyCar3.RentCar();
+            economyCar3.ReturnCar();
 
             Car economyCar4 = new Car("Ford", "Focus", 320.5f, true);
-            economyCar4.GetRentCar();
-            economyCar4.SetRentCar("Model G");
-            economyCar4.GetReturnCar();
-            economyCar4.SetReturnCar("Model G");
+            economyCar4.RentCar();
+            economyCar4.ReturnCar();
 
             Car economyCar5 = new Car("Nissan", "Sentra", 350.8f, false);
-            economyCar5.GetRentCar();
-            economyCar5.SetRentCar("Model H");
-            economyCar5.GetReturnCar();
-            economyCar5.SetReturnCar("Model H");
-
+            economyCar5.RentCar();
+            economyCar5.ReturnCar();
 
             Console.WriteLine("Luxury Cars:");
             luxuryCar.GetDetails();
