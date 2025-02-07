dc2843e8899d85eb1399b1b2a8e16b00956003c0
IshikaBacancy <ishika.raiyani@bacancy.com>
2025-02-07 17:11:17 +0530

    Create Program.cs

---
+++ OOP Assigment 3/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace OOP_3_Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ITaxCalculator standardTax = new StandardTaxCalculator();
+            Invoice invoice = new Invoice(1000, standardTax);
+            invoice.PrintInvoice();
+
+
+        }
+    }
+}
