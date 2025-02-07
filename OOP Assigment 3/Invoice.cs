19c9e250c6e89476b4c31ca6b0659134e0ed97b8
IshikaBacancy <ishika.raiyani@bacancy.com>
2025-02-07 17:13:08 +0530

    Update Invoice.cs

--- OOP Assigment 3/Invoice.cs
+++ OOP Assigment 3/Invoice.cs
@@ -6,35 +6,6 @@ using System.Threading.Tasks;
 
 namespace OOP_3_Assignment
 {
-    //public class Invoice
-    //{
-    //    private double _amount;
-
-    //    public Invoice(double amount)
-    //    {
-    //        this._amount = amount;
-    //    }
-
-    //    public double CalculateTotal()
-    //    {
-    //        return _amount * 1.18;
-    //    }
-
-    //    // Creating a new class for invoice printer to follow Single Responsibility Principle
-    //    public class InvoicePrinter
-    //    {
-    //        public void PrintInvoice(Invoice invoice)
-    //        {
-    //            Console.WriteLine($"Invoice Total: {invoice.CalculateTotal()}");
-    //        }
-    //    }
-
-
-    //}
-
-   
-
-    // Modify the Invoice class to use ITaxCalculator
     public class Invoice
     {
         private double amount;
@@ -46,7 +17,6 @@ namespace OOP_3_Assignment
             this.taxCalculator = taxCalculator;
         }
 
-        // Calculate total using the specified tax calculator
         public double CalculateTotal()
         {
             return amount + taxCalculator.CalculateTax(amount);
